cc4e6b096dc96388190b02f3eeaf575f6ab586db
Jhonny <159505052+Jhonny-Almeida@users.noreply.github.com>
2026-04-30 16:35:54 -0400

    inplementação do que foi apresentado na documentação

---
+++ aula23.04-main/lab-mysql-docker/app/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const session = require('express-session');
+const usuariosRoutes = require('./routes/usuarios');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'segredo-lab',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.use('/auth', authRoutes);
+app.use('/usuarios', usuariosRoutes);
+
+app.listen(3000, () => {
+  console.log('API rodando na porta 3000');
+});
